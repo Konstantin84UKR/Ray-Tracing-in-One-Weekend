b3e797e3668214181ff7d37580bda97db9063a8f
Konstantin Bondarenko <43937942+Konstantin84UKR@users.noreply.github.com>
2020-12-06 17:45:34 +0200

    Add files via upload

---
+++ part3/src/ray.js
@@ -0,0 +1,28 @@
+
+//import { __esModule } from './gl-matrix.js';
+//import { sayHi, sayBye } from './say.js';
+import * as say from './say.js';
+
+export default class Ray {
+    constructor(origin, direction) {
+        this.origin = origin;
+        this.direction = direction;
+        this.t = 1;
+    }
+    // get origin() {
+    //     return this.origin;
+
+    // }
+    // get direction() {
+    //     return this.direction;
+    // }
+
+    getAt(t) {
+        //say.sayHi('fgdg');
+        let v = glMatrix.vec3.create();
+        glMatrix.vec3.scale(v, this.direction, t);
+        glMatrix.vec3.add(v, this.origin, v);
+        return v;
+    }
+
+}
